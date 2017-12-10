694a730f84aa84b5e8166cb6c38df239cb5d3de2
Viciana <jairo.viciana@accenture.com>
2017-12-10 18:17:29 +0100

    more reactions

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { Clock } from "./clock";
 import { ColorChanger } from "./colorChanger";
+import { Store } from "../State/store";
+import { observer } from "mobx-react";
 
+@observer
 export class App extends React.Component {
   render() {
     return (
@@ -9,7 +12,7 @@ export class App extends React.Component {
         <h2>Hello World!!</h2>
         <div>
           <ColorChanger />
-          Here you have a Clock: <Clock />
+          Here you have a {Store.color} Clock: <Clock />
         </div>
       </div>
     );
