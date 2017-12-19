aa4e12da89fc75da408a7dc4223d85ed3a843519
Viciana <jairo.viciana@accenture.com>
2017-12-19 16:07:41 +0100

    example for testing inputs and state

--- src/components/colorChanger.test.tsx
+++ src/components/colorChanger.test.tsx
@@ -2,8 +2,18 @@ import * as React from "react";
 import { shallow } from "enzyme";
 
 import { ColorChanger } from "./colorChanger";
+import { Store } from "../state/store";
 
 test("<ColorChanger />", () => {
   const wrapper = shallow(<ColorChanger />);
+  const input = wrapper.find("input");
+
+  input.simulate("change", { currentTarget: { value: "red" } });
+
+  expect(Store.color.tempValue).toBe("red");
+
+  wrapper.find("button").simulate("click");
+
+  expect(Store.color.value).toBe("red");
   expect(wrapper).toMatchSnapshot();
 });
